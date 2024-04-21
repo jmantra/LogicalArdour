a3cb327304942e43e2a25381674f0ba4e1506a6d
jmantra <justin.ehrlichman@gmail.com>
2024-04-20 21:11:35 -0400

    Now launches directly into Musescore

--- mscoreex.lua
+++ mscoreex.lua
@@ -3,23 +3,20 @@ ardour {
 	name = "Musescore - Open Midi region in musical notation format",
 	author      = "Justin Ehrlichman",
 description = [[
-Takes a selected MIDI region, converts it to a PDF using musescore then opens up the PDF in Firefox
+Takes a selected MIDI region and opens it in Musescore
 ]]
 }
 
 function factory () return function (signal, ...)
 
-local md = LuaDialog.Message ("Open Musical Score", "If Firefox shows a blank page, you may have to wait a minute or two for Ardour to write the file to disk", LuaDialog.MessageType.Info, LuaDialog.ButtonType.Close)
+local md = LuaDialog.Message ("Open Musical Score", "If Musescore shows a blank page, you may have to wait a minute or two for Ardour to write the file to disk", LuaDialog.MessageType.Info, LuaDialog.ButtonType.Close)
 	print (md:run())
 
 	md = nil
 	collectgarbage ()
 
-	local md = LuaDialog.Message ("Open Musical Score", "To continue using Ardour you will need to close the Firefox window or the tab the score is open in", LuaDialog.MessageType.Info, LuaDialog.ButtonType.Close)
-	print (md:run())
 
-	md = nil
-	collectgarbage ()
+
 
 
 
@@ -58,15 +55,86 @@ local quotedfilepath = '"' .. filepath .. '"'
 
 local command = "mscore -o /tmp/output.pdf  " ..quotedfilepath
 
-os.execute(command)
+-- Open the file "mscore.sh" for writing
+local file = io.open("mscore.sh", "w")
 
-os.execute("firefox file:///tmp/output.pdf")
+
+
+if not file then
+    -- Handle error if file couldn't be opened
+    print("Error: Failed to create file 'mscore.sh'")
+else
+    -- Write the command content to the file
+
+
+    file:write(command .. "\n") -- Write the command to the file
+
+    -- Close the file
+    file:close()
+
+    print("File 'mscore.sh' created successfully!")
+
+    -- Delete the file after printing the success message
+
+end
+
+local mv = "mv mscore.sh /tmp/mscore.sh"
+
+os.execute(mv)
+
+os.forkexec("/bin/bash", "/tmp/mscore.sh")
+
+--os.execute("firefox file:///tmp/output.pdf")
+
+ os.remove("/tmp/mscore.sh")
+
+
+
+-- Open the file "mscore.sh" for writing
+local file = io.open("mscore.sh", "w")
+
+
+
+if not file then
+    -- Handle error if file couldn't be opened
+    print("Error: Failed to create file 'mscore.sh'")
+else
+    -- Write the command content to the file
+ local command = "mscore " ..quotedfilepath
+
+    file:write(command .. "\n") -- Write the command to the file
+
+    -- Close the file
+    file:close()
+
+    print("File 'mscore.sh' created successfully!")
+
+    -- Delete the file after printing the success message
+
+end
+
+local mv = "mv mscore.sh /tmp/mscore.sh"
+
+os.execute(mv)
+
+os.forkexec("/bin/bash", "/tmp/mscore.sh")
+
+
+
+
+
+
 
 
 
 
 
 end end
+
+
+
+
+
 
 
 
