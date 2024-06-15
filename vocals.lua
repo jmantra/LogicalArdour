85de6c9ff0154744f7b13137fb56a735d0c90ada
jmantra <justin.ehrlichman@gmail.com>
2024-06-15 14:29:47 -0400

    added compressed vocals

--- vocals.lua
+++ vocals.lua
@@ -12,7 +12,7 @@ local dialog_options = {
   {
    type = "dropdown", key = "dropdown", title = "Choose Vocal Preset", values =
    {
-    ["Choose Vocal Preset"] = 1, ["Classic"] = 2,["Bright"] = 3, ["Dance"] = 4
+    ["Choose Vocal Preset"] = 1, ["Classic"] = 2,["Bright"] = 3, ["Dance"] = 4, ["Compressed"] = 5
 
    },
    default = "Choose Vocal Preset"
@@ -270,6 +270,83 @@ proc:load_preset(preset)
 
 
 -- *** End of dance vocals ****
+end
+
+if rv and rv["dropdown"] == 5 then
+
+apply_preset_to_tracks("compressed")
+
+-- Classic Vocals
+
+add_plugin_to_selected_tracks("ACE EQ", ARDOUR.PluginType.LV2, 0) --first EQ of the chain
+
+local proc = Session:route_by_name("compressed"):to_track():nth_plugin(0):to_insert():plugin(0)
+
+-- Get the preset
+local preset = proc:preset_by_label("compressed")
+
+-- Load the preset
+proc:load_preset(preset)
+
+-- ACE Compressor
+
+add_plugin_to_selected_tracks("ACE Compressor", ARDOUR.PluginType.LV2, 1) --first EQ of the chain
+
+local proc = Session:route_by_name("compressed"):to_track():nth_plugin(1):to_insert():plugin(1)
+
+-- Get the preset
+local preset = proc:preset_by_label("compressed")
+
+-- Load the preset
+proc:load_preset(preset)
+
+add_plugin_to_selected_tracks("ACE Compressor", ARDOUR.PluginType.LV2, 2) --first EQ of the chain
+
+local proc = Session:route_by_name("compressed"):to_track():nth_plugin(2):to_insert():plugin(2)
+
+-- Get the preset
+local preset = proc:preset_by_label("compressed-2")
+
+-- Load the preset
+proc:load_preset(preset)
+
+
+
+add_plugin_to_selected_tracks("Comb Splitter", ARDOUR.PluginType.LV2, 3) --first EQ of the chain
+
+local proc = Session:route_by_name("compressed"):to_track():nth_plugin(3):to_insert():plugin(3)
+
+-- Get the preset
+local preset = proc:preset_by_label("compressed")
+
+-- Load the preset
+proc:load_preset(preset)
+
+-- GxReverb-Stereo
+
+add_plugin_to_selected_tracks("GxCompressor", ARDOUR.PluginType.LV2, 4) --first EQ of the chain
+
+local proc = Session:route_by_name("compressed"):to_track():nth_plugin(4):to_insert():plugin(4)
+
+-- Get the preset
+local preset = proc:preset_by_label("compressed")
+
+-- Load the preset
+proc:load_preset(preset)
+
+add_plugin_to_selected_tracks("ACE EQ", ARDOUR.PluginType.LV2, 5) --first EQ of the chain
+
+local proc = Session:route_by_name("compressed"):to_track():nth_plugin(5):to_insert():plugin(5)
+
+-- Get the preset
+local preset = proc:preset_by_label("compressed-2")
+
+-- Load the preset
+proc:load_preset(preset)
+
+
+
+-- *** End of compressed vocals ****
 end
 
 
