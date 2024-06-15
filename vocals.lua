5a7a7e8be74d7fd77db3d3b418308d012a097cd0
jmantra <justin.ehrlichman@gmail.com>
2024-06-14 22:22:50 -0400

    added menus and other vocal presets

--- vocals.lua
+++ vocals.lua
@@ -8,26 +8,45 @@ ardour {
 
 function factory () return function ()
 
--- Code block to clear all plugins on the track and change name
+local dialog_options = {
+  {
+   type = "dropdown", key = "dropdown", title = "Choose Vocal Preset", values =
+   {
+    ["Choose Vocal Preset"] = 1, ["Classic"] = 2,["Bright"] = 3, ["Dance"] = 4
 
-local sel = Editor:get_selection ()
-local plugin_name = "classic"
-	-- for each selected track/bus
-	for r in sel.tracks:routelist ():iter () do
-		local plugs = ARDOUR.ProcessorList(); -- create a PluginList
-		local i = 0;
-		repeat -- iterate over all plugins/processors
-			local proc = r:nth_processor (i)
-			if not proc:isnil () then
-				-- append plugin to list
-				plugs:push_back(proc)
-			end
-			i = i + 1
-		until proc:isnil ()
+   },
+   default = "Choose Vocal Preset"
+  }
+ }
 
-		r:remove_processors (plugs, nil)
-		r:set_name(plugin_name, nil)
-	end
+ local od = LuaDialog.Dialog ("Choose Vocal Preset", dialog_options)
+ local rv = od:run()
+
+-- Function to clear out all plugins and name the track
+
+function apply_preset_to_tracks(preset_name)
+    local sel = Editor:get_selection ()
+    -- for each selected track/bus
+    for r in sel.tracks:routelist ():iter () do
+        local plugs = ARDOUR.ProcessorList(); -- create a PluginList
+        local i = 0;
+        repeat -- iterate over all plugins/processors
+            local proc = r:nth_processor (i)
+            if not proc:isnil () then
+                -- append plugin to list
+                plugs:push_back(proc)
+            end
+            i = i + 1
+        until proc:isnil ()
+
+        r:remove_processors (plugs, nil)
+        r:set_name(preset_name, nil)
+    end
+end
+
+-- Example usage:
+
+
 
 -- Create a function to add plugins
 
@@ -42,8 +61,9 @@ function add_plugin_to_selected_tracks(plugin_name, plugin_type, position)
         end
     end
 end
+if rv and rv["dropdown"] == 2 then
 
-
+apply_preset_to_tracks("classic")
 
 -- Classic Vocals
 
@@ -103,9 +123,160 @@ local preset = proc:preset_by_label("classic")
 -- Load the preset
 proc:load_preset(preset)
 
+
+
+-- *** End of classic vocals ****
+end
+
+if rv and rv["dropdown"] == 3 then
+
+apply_preset_to_tracks("bright")
+
+-- Classic Vocals
+
+add_plugin_to_selected_tracks("ACE EQ", ARDOUR.PluginType.LV2, 0) --first EQ of the chain
+
+local proc = Session:route_by_name("bright"):to_track():nth_plugin(0):to_insert():plugin(0)
+
+-- Get the preset
+local preset = proc:preset_by_label("bright")
+
+-- Load the preset
+proc:load_preset(preset)
+
+-- ACE Compressor
+
+add_plugin_to_selected_tracks("ACE Compressor", ARDOUR.PluginType.LV2, 1) --first EQ of the chain
+
+local proc = Session:route_by_name("bright"):to_track():nth_plugin(1):to_insert():plugin(1)
+
+-- Get the preset
+local preset = proc:preset_by_label("bright")
+
+-- Load the preset
+proc:load_preset(preset)
+
+add_plugin_to_selected_tracks("Calf Exciter", ARDOUR.PluginType.LV2, 2) --first EQ of the chain
+
+local proc = Session:route_by_name("bright"):to_track():nth_plugin(2):to_insert():plugin(2)
+
+-- Get the preset
+local preset = proc:preset_by_label("bright")
+
+-- Load the preset
+proc:load_preset(preset)
+
+
+
+add_plugin_to_selected_tracks("TAP DeEsser", ARDOUR.PluginType.LADSPA, 3) --first EQ of the chain
+
+local proc = Session:route_by_name("bright"):to_track():nth_plugin(3):to_insert():plugin(3)
+
+-- Get the preset
+local preset = proc:preset_by_label("bright")
+
+-- Load the preset
+proc:load_preset(preset)
+
 -- GxReverb-Stereo
+
+add_plugin_to_selected_tracks("ACE EQ", ARDOUR.PluginType.LV2, 4) --first EQ of the chain
+
+local proc = Session:route_by_name("bright"):to_track():nth_plugin(4):to_insert():plugin(4)
+
+-- Get the preset
+local preset = proc:preset_by_label("bright-2")
+
+-- Load the preset
+proc:load_preset(preset)
+
+
+
+-- *** End of bright vocals ****
+end
+
+if rv and rv["dropdown"] == 4 then
+
+apply_preset_to_tracks("dance")
+
+-- Classic Vocals
+
+add_plugin_to_selected_tracks("ACE EQ", ARDOUR.PluginType.LV2, 0) --first EQ of the chain
+
+local proc = Session:route_by_name("dance"):to_track():nth_plugin(0):to_insert():plugin(0)
+
+-- Get the preset
+local preset = proc:preset_by_label("dance")
+
+-- Load the preset
+proc:load_preset(preset)
+
+-- ACE Compressor
+
+add_plugin_to_selected_tracks("ACE Compressor", ARDOUR.PluginType.LV2, 1) --first EQ of the chain
+
+local proc = Session:route_by_name("dance"):to_track():nth_plugin(1):to_insert():plugin(1)
+
+-- Get the preset
+local preset = proc:preset_by_label("dance")
+
+-- Load the preset
+proc:load_preset(preset)
+
+add_plugin_to_selected_tracks("Calf Exciter", ARDOUR.PluginType.LV2, 2) --first EQ of the chain
+
+local proc = Session:route_by_name("dance"):to_track():nth_plugin(2):to_insert():plugin(2)
+
+-- Get the preset
+local preset = proc:preset_by_label("dance")
+
+-- Load the preset
+proc:load_preset(preset)
+
+
+
+add_plugin_to_selected_tracks("ACE Reverb", ARDOUR.PluginType.LV2, 3) --first EQ of the chain
+
+local proc = Session:route_by_name("dance"):to_track():nth_plugin(3):to_insert():plugin(3)
+
+-- Get the preset
+local preset = proc:preset_by_label("dance")
+
+-- Load the preset
+proc:load_preset(preset)
+
+-- GxReverb-Stereo
+
+add_plugin_to_selected_tracks("GxChorus-Stereo", ARDOUR.PluginType.LV2, 4) --first EQ of the chain
+
+local proc = Session:route_by_name("dance"):to_track():nth_plugin(4):to_insert():plugin(4)
+
+-- Get the preset
+local preset = proc:preset_by_label("dance")
+
+-- Load the preset
+proc:load_preset(preset)
+
+add_plugin_to_selected_tracks("GxChorus-Stereo", ARDOUR.PluginType.LV2, 5) --first EQ of the chain
+
+local proc = Session:route_by_name("dance"):to_track():nth_plugin(5):to_insert():plugin(5)
+
+-- Get the preset
+local preset = proc:preset_by_label("dance-2")
+
+-- Load the preset
+proc:load_preset(preset)
+
+
+
+-- *** End of dance vocals ****
+end
+
+
+
 
 
 
 end end
+
 
