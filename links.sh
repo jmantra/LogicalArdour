54f5aa387d15779ba571133564c87976b478f41a
jmantra <justin.ehrlichman@gmail.com>
2024-08-25 12:13:37 -0400

    Add files via upload

---
+++ links.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+ln -s '/opt/LogicalArdour/Dance Drums/Dance Drums.sfz' '$HOME/.lv2/sfizz_Dance_Drums.lv2/Dance Drums.sfz'
+
+ln -s '/opt/LogicalArdour/Jazz Drums/Jazz Drums.sfz' '$HOME/.lv2/sfizz_Jazz_Drums.lv2/Jazz Drums.sfz'
+
+ln - s '/opt/LogicalArdour/Electronic/Electronic Drums.sfz' '$HOME/.lv2/sfizz_Electronic_Drums.lv2/Electronic Drums.sfz'
+
+ln - s '/opt/LogicalArdour/Power Drums/Power Drums.sfz' '$HOME/.lv2/sfizz_Power_Drums.lv2/Power Drums.sfz'
+
+
+
+
