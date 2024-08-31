0db68665e29d3fc13a78885ca1b321a768f66711
jmantra <justin.ehrlichman@gmail.com>
2024-08-31 14:35:36 -0400

    Add files via upload

---
+++ fedora-install.sh
@@ -0,0 +1,5 @@
+sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+
+$ sudo dnf install gstreamer1-plugins-{bad-\*,good-\*,base} gstreamer1-plugin-openh264 gstreamer1-libav --exclude=gstreamer1-plugins-bad-free-devel
+
+sudo dnf install lame* --exclude=lame-devel
