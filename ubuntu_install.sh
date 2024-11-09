ed82128f69e414f71afad00c5007dfdd9695c5ad
jmantra <justin.ehrlichman@gmail.com>
2024-11-09 15:55:09 -0500

    Update ubuntu_install.sh

--- ubuntu_install.sh
+++ ubuntu_install.sh
@@ -209,6 +209,12 @@ for file in $HOME/.config/ardour8/route_templates/*; do
     fi
 done
 
+for file in $HOME/.config/MuseScore/*; do
+    if [ -f "$file" ]; then
+        awk -v home="$HOME" '{gsub("/home/justin", home); print}' "$file" > tmp && mv tmp "$file"
+    fi
+done
+
 
 
 #DIR=$(find /opt -maxdepth 1 -type d -name "Ardour-*" | sort -V | tail -n 1)
