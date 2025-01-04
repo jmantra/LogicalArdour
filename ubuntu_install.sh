fbdb03f855c85e0ccc0d7b624eba144f3b7c4c38
jmantra <justin.ehrlichman@gmail.com>
2025-01-04 17:51:05 -0500

    Add files via upload

--- ubuntu_install.sh
+++ ubuntu_install.sh
@@ -42,6 +42,8 @@ while true; do
       cd $HOME
     sudo apt install build-essential git libboost-all-dev gcc g++ pkg-config libasound2-dev libgtk2.0-dev libglibmm-2.4-dev libsndfile1-dev libarchive-dev liblo-dev libtag1-dev vamp-plugin-sdk librubberband-dev libfftw3-dev libaubio-dev libxml2-dev libsamplerate0-dev lv2-dev libserd-dev libsord-dev libsratom-dev liblilv-dev libgtkmm-2.4-dev libjack-jackd2-dev libogg-dev libcppunit-dev libudev-dev libwebsockets-dev libusb-dev libsuil-dev libdbus-1-dev xjadeo libusb-1.0-0-dev libreadline-dev  libarchive-dev liblo-dev libtag1-dev libvamp-sdk2v5 librubberband-dev libaubio-dev libpangomm-1.4-dev libserd-dev libsord-dev libsratom-dev liblilv-dev libgtkmm-2.4-dev libsuil-dev libcppunit-dev python3 liblrdf0-dev libraptor2-dev python-is-python3 git -y
 wget https://jmantra.blob.core.windows.net/data/ardour.zip
+
+unzip ardour.zip
  #   git clone https://github.com/Ardour/ardour.git
     cd ardour
   #  git checkout 8.10
